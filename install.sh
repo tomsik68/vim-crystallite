38fb65543adbf9929f13b8ae447df8ca4dc73629
kyrose <kyrose@users.noreply.github.com>
2016-06-02 00:33:11 +0800

    Updated

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ mkdir -p ~/.vim/colors/
 cp colors/Crystallite.vim ~/.vim/colors/
 echo "color Crystallite" >> ~/.vimrc
 echo -e "\nDone. Open vim to see the new colorscheme.\n"
-echo -e Run \'sed -i -e s/"color Crystallite"/""/g ~/.vimrc\' to remove Crystallite as default Vim colorscheme
+echo "To uninstall: Open ~/.vimrc with any text editor & delete the line with \"color Crystallite\""
