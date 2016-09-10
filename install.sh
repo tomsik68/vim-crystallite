731fbb6f5a3ca0ef7aa25df8eb60be642ae0015f
itsabear <itsabear@cpan.org>
2016-09-10 12:52:48 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,4 +4,4 @@ mkdir -p ~/.vim/colors/
 cp colors/Crystallite.vim ~/.vim/colors/
 echo "color Crystallite" >> ~/.vimrc
 echo -e "\nDone. Open vim to see the new colorscheme.\n"
-echo "To uninstall: Open ~/.vimrc with any text editor & delete the line with \"color Crystallite\""
+echo "To uninstall, just run this: replace \"color Crystallite\" \"\" -- ~/.vimrc"
