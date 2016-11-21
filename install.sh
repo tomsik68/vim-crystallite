11eca80f5475f9f5e7bc653fef5c3f5a0dcc9d6f
Dusan Baran <dbaran@hotmail.sk>
2016-11-21 22:51:21 +0100

    Modified install.sh to be more flexible
    install.sh can now find vimrc in 2 places
    home '~/.vimrc' and home/.vim '~/.vim/vimrc'

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
+VIMRCPATH='~/.vimrc'
+if [ -f '~/.vim/vimrc' ]; then VIMRCPATH='~/.vim/vimrc'; fi
+echo found vimrc in $VIMRCPATH
 mkdir -p ~/.vim/colors/
 cp colors/Crystallite.vim ~/.vim/colors/
-echo "color Crystallite" >> ~/.vimrc
-echo -e "\nDone. Open vim to see the new colorscheme.\n"
-echo "To uninstall, run this: replace \"color Crystallite\" \"\" -- ~/.vimrc; rm ~/.vim/colors/Crystallite.vim"
+echo 'color Crystallite' >> $VIMRCPATH
+echo -e '\nDone. Open vim to see the new colorscheme.\n'
+echo "To uninstall, run this: replace \'color Crystallite\' \'\' -- ~/.vimrc; rm ~/.vim/colors/Crystallite.vim"
